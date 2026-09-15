1219d9d411cb429d39d03d21a0483ff9cce22c79
Apoorvan21 <161784936+Apoorvan21@users.noreply.github.com>
2026-09-15 18:25:28 +0530

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,10 +1,21 @@
+import Layer
+
 class NeuralNetwork:
   def __init__(self,layer_size):
     self.layers = []
     for i in range(len(layer_size)-1):
       self.layers.append(Layer(layer_size[i],layer_size[i+1]))
-  
+
   def forward(self,x):
     for layer in self.layers:
       x = layer.forward(x)
     return x
+
+  def backward(self,dL_dz):
+    for layer in reversed(self.layers):
+      dL_dz = layer.backward(dL_dz)
+      print(dL_dz)
+
+  def update(self,a):
+    for layer in self.layers:
+      layer.update(a)
