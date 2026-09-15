7c5530f9b89338dcce8c996e482a85c83eb75879
Apoorvan21 <161784936+Apoorvan21@users.noreply.github.com>
2026-09-15 18:27:17 +0530

    Implement loss function and training method
    Added loss function and training loop for neural network.

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def lossFunction(y,y_pred):
+  avg = 0
+  for i in range(len(y)):
+    avg += (y[i] - y_pred[i])**2
+  loss = (0.5) * avg/len(y)
+
+  return loss
+
+def train(nn,x,y,epochs,a):
+  losses = []
+  for i in range(epochs):
+    y_pred = nn.forward(x)
+    loss = lossFunction(y,y_pred)
+    losses.append(loss)
+    dL_dz = (y_pred - y)
+    nn.backward(dL_dz)
+    nn.update(a)
+  return losses
+
 n = NeuralNetwork([2,4,5,1])
 x = np.array([0.25,0.42])
 y = np.array([2.1])
 
-loss = (n.forward(x)-y)**2
+print(nn.forward(x))
 
+train(nn,x,y,100,0.1)
+
+print(nn.forward(x))
