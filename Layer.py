029b8ca8fe5c2401e032473a784fe02dce0ab707
Apoorvan21 <161784936+Apoorvan21@users.noreply.github.com>
2026-09-15 18:26:00 +0530

    Update Layer.py

--- Layer.py
+++ Layer.py
@@ -1,6 +1,6 @@
 class Layer:
   def __init__(self,inp_size,out_size):
-    self.W = np.random.randn(out_size,inp_size)
+    self.W = np.random.randn(out_size,inp_size) *0.01
     self.b = np.zeros(out_size)
     self.X = None
     self.dW = None
@@ -9,3 +9,12 @@ class Layer:
   def forward(self,X):
     self.X = X
     return self.W @ X + self.b
+
+  def backward(self,dL_dz):
+    self.db = dL_dz
+    self.dW = np.outer(dL_dz,self.X)
+    return self.W.T @ dL_dz
+
+  def update(self,a):
+    self.W -= a * self.dW
+    self.b -= a * self.db
